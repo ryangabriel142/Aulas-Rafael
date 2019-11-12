e8073d5e65a39c14b24b8c94ab65d0bf2edd693d
Ryan Gabriel <48722498+ryangabriel142@users.noreply.github.com>
2019-11-12 11:54:11 -0300

    Add files via upload

--- AcessoBancoDados/model/Pessoa.php
+++ AcessoBancoDados/model/Pessoa.php
@@ -50,30 +50,9 @@ class Pessoa{
     while($temp = $retornoBanco->fetch_array()) {
       $dados[]=$temp;
     }
-    echo "Foram encontrados " . count($dados) . " registros.";//verifica quantos registros tem no vetor
-    if (count($dados) > 0){
-      ?>
-      <table border=1>
-        <tr>
-          <td>Id</td>
-          <td>Nome</td>
-          <td>E-mail</td>
-          <td> Data Nascimento </td>
-          <td> Data Cadastro </td>
-        </tr>
-        <?php
-        foreach ($dados as $key => $value) {
-          echo "<tr><td>" . $value["id"];
-          echo "</td><td>" . $value["nome"];
-          echo "</td><td>" . $value["email"];
-          echo "</td><td>" . $value["dataNascimento"];
-          echo "</td><td>" . $value["dataCadastro"];
-          echo "</td></tr>";
-        }
-        ?>
-      </table>
-      <?php
-    }
+    return $dados;
+
+    
   }
 }
 ?>
