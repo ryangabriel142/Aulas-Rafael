04b5fca4a08d3be3a408b98b138eb397e7c08958
Ryan Gabriel <48722498+ryangabriel142@users.noreply.github.com>
2019-11-12 11:52:04 -0300

    Add files via upload

---
+++ AcessoBancoDados/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <script>
+    window.location.href = "view/index.php";
+    </script>
+  </body>
+</html>
