dac4658403d60f228345d77841d8a743b1d3a325
Ryan Gabriel <48722498+ryangabriel142@users.noreply.github.com>
2019-11-12 11:54:44 -0300

    Add files via upload

--- AcessoBancoDados/view/formulario.php
+++ AcessoBancoDados/view/formulario.php
@@ -5,12 +5,21 @@
     <title></title>
   </head>
   <body>
+    <?php
+    if(isset($_GET["mensagem"]) & !empty($_GET["mensagem"])) {
+      if ($_GET["mensagem"]== "sucesso"){
+        echo "Dados gravados com sucesso!!!";
+      }elseif ($_GET["mensagem"]=="erro") {
+        echo "Erro ao gravar os dados";
+      }
+      }
+    ?>
     <form action="../controler/controle.php" method="post">
       <label>Nome: </label>
       <input type="text" name="nome" value="">
       <label>E-mail:</label>
-      <label>Data Nascimento:</label>
       <input type="date" name="dataNascimento" value="">
+      <label>Data Nascimento:</label>
       <input type="email" name="email" value="">
       <input type="submit" name="" value="Enviar">
     </form>
---
+++ AcessoBancoDados/view/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ul>
+      <li><a href="formulario.php">Cadastro</a></li>
+      <li><a href="../controler/listarPessoa.php">Listar</a></li>
+    </ul>
+  </body>
+</html>
